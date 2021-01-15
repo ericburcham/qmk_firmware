0bdb0824df2db58e5fc4865833f9cd108a03fbd5
Eric Burcham <jameseburcham@gmail.com>
2021-01-15 10:06:37 -0600

    added script to build all my custom keymaps

---
+++ build-keebs.sh
@@ -0,0 +1,5 @@
+qmk compile -kb crkbd/rev1/common -km ericburcham
+qmk compile -kb kyria/rev1 -km ericburcham
+qmk compile -kb lily58/rev1 -km ericburcham
+qmk compile -kb sofle/rev1 -km ericburcham
+
