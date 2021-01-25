ae7c43e612f4a21438b4353b1c100d2f199ea33b
Eric Burcham <eburcham@eprod.com>
2021-01-25 15:16:52 -0600

    include refresh scripts for Windows and Linux.

---
+++ refresh.ps1
@@ -0,0 +1,13 @@
+git fetch --all
+git checkout master
+git pull
+git merge upstream/master
+git push
+git checkout main
+git pull
+git merge master
+git push
+git checkout develop
+git pull
+git merge main
+git push
